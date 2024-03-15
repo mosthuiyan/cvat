7114fbd68f73096500531abd9ec338df8cbac474
huiyan <huiyanliu@buaa.edu.cn>
2024-03-15 15:14:23 +0800

    annotation: remove blank

--- cvat-ui/webpack.config.js
+++ cvat-ui/webpack.config.js
@@ -13,7 +13,6 @@ module.exports = (env) => {
     const defaultAppConfig = path.join(__dirname, 'src/config.tsx');
     const defaultPlugins = ['plugins/sam'];
 
-    //
     const sourceMapsDisabled = (process.env.DISABLE_SOURCE_MAPS || 'false').toLocaleLowerCase() === 'true';
     const appConfigFile = process.env.UI_APP_CONFIG ? process.env.UI_APP_CONFIG : defaultAppConfig;
     const pluginsList = process.env.CLIENT_PLUGINS ? [...defaultPlugins, ...process.env.CLIENT_PLUGINS.split(':')]
